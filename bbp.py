37230ca42dccbe435fb686e7ebdfe866db98ac99
Filip Rus <filiprusgm@gmail.com>
2024-06-14 15:27:03 +0200

    dodal bbp

---
+++ bbp.py
@@ -0,0 +1,29 @@
+import time
+#bbp algoritem
+def S(j, n):
+    # Left sum
+    s = 0.0
+    k = 0
+    while k <= n:
+        r = 8*k+j
+        s = (s + float(pow(16, n-k, r)) / r) % 1.0
+        k += 1
+    # Right sum
+    t = 0.0
+    k = n + 1
+    while 1:
+        newt = t + pow(16, n-k) / (8*k+j)
+        # Iterate until t no longer changes
+        if t == newt:
+            break
+        else:
+            t = newt
+        k += 1
+    return s + t
+
+def bbp(n):
+    start = time.time()
+    n -= 1
+    x = (4*S(1, n) - 2*S(4, n) - S(5, n) - S(6, n)) % 1.0
+    end = time.time()
+    return ("%014x" % int(x * 16**14),end - start)
